e73a0a734318fa66cdbd95935b21ffa34fe1851f
Foo-Manroot <miguel.garciamartin@edu.uah.es>
2017-02-17 08:44:00 +0100

    Directorios reorganizados

---
+++ src/candy.c
@@ -0,0 +1,208 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define FIL 5
+#define COL 5
+
+/* Estructura Diamante */
+typedef struct {
+	
+	//id = {1,2,3,4,5,6,7,8} indican el tipo de diamante
+	//id = {0} indica que no hay fiamante en ese hueco
+	int id;
+   
+  } Diamante;
+
+void mostrar_malla(Diamante malla[FIL][COL]){
+	for (int i = 0; i < FIL; ++i)
+	{
+		for (int j = 0; j < COL; ++j)
+		{
+			printf("%d ",malla[i][j].id);
+		}
+		printf("\n");
+	}
+	
+}
+
+Diamante generar_diamante(){
+	Diamante d;
+
+	d.id = (rand() % 9) + 1;
+
+	return d;
+}
+
+Diamante cambiar_diamante(int num){
+	/*Si se pasa por parametro num = 0, seria como eliminar un dimante*/
+	Diamante d;
+
+	d.id = num;
+	
+	return d;
+}
+
+void iniciar_malla(Diamante malla[FIL][COL]){
+	
+	for (int i = 0; i < FIL; ++i)
+	{
+		for (int j = 0; j < COL; ++j)
+		{
+			malla[i][j].id = 0; 
+		}
+	}	
+}
+
+void rellenar_malla(Diamante malla[FIL][COL]){
+	for (int i = 0; i < FIL; ++i)
+	{
+			for (int j = 0; j < COL; ++j)
+			{
+			malla[i][j]= generar_diamante(); 
+			}
+	}	
+}
+
+int es_valido_mov(int posY, int posX, int mov, Diamante malla[FIL][COL]){
+	/*Si devuelve 0 no hay error*/
+	int b = 1;
+	switch(mov){
+		case 0:
+			/*Mover hacia abajo*/
+			if(posY < (FIL-1)) b = 0;
+			break;
+		case 1:
+			/*Mover hacia arriba*/
+			if(posY > 0) b = 0;
+			break;
+		case 2:
+			/*Mover hacia izquierda*/
+			if(posX > 0) b = 0;
+			break;
+		case 3:
+			/*Mover hacia derecha*/
+			if(posX < (COL-1)) b = 0;
+			break;
+	}
+	return b;
+}
+
+void mover_diamante(int posY, int posX, int mov, Diamante malla[FIL][COL]){
+	Diamante aux;
+
+	if (es_valido_mov(posY,posX,mov,malla) == 0){
+
+		switch(mov){
+		case 0:
+			/*Mover hacia abajo*/
+			aux = malla[posY + 1][posX];
+			malla[posY + 1][posX] = malla[posY][posX];
+			malla[posY][posX] = aux;
+
+			break;
+		case 1:
+			/*Mover hacia arriba*/
+			aux = malla[posY - 1][posX];
+			malla[posY - 1][posX] = malla[posY][posX];
+			malla[posY][posX] = aux;
+			break;
+		case 2:
+			/*Mover hacia izquierda*/
+			aux = malla[posY][posX - 1];
+			malla[posY][posX - 1] = malla[posY][posX];
+			malla[posY][posX] = aux;
+			break;
+		case 3:
+			/*Mover hacia derecha*/
+			aux = malla[posY ][posX + 1];
+			malla[posY][posX + 1] = malla[posY][posX];
+			malla[posY][posX] = aux;
+			break;
+		}
+	}else printf("Error: movimeinto no valido\n");
+}
+
+int son_iguales(Diamante d1,Diamante d2){
+	//Compara si dos diamantes son del mismo tipo
+	int b = 1;
+	if ((d1.id != 0)&&(d1.id == d2.id)) b = 0;
+	return b;
+}
+
+void buscar_coincidencias(Diamante malla[FIL][COL]){
+	for (int i = 0; i < FIL; ++i){
+	
+		for (int j = 0; j < COL; ++j)
+		{
+			Diamante d_cen = malla[i][j];
+			Diamante d_der = malla[i][j + 1];
+			Diamante d_izq = malla[i][j - 1];
+
+			if((son_iguales(d_cen,d_der) == 0)&&
+			(son_iguales(d_cen,d_izq) == 0)) {
+			/*Se eliminan los diamantes cercanos que sean iguales*/
+
+			malla[i][j] = cambiar_diamante(0);
+			malla[i][j + 1] = cambiar_diamante(0);
+			malla[i][j - 1] = cambiar_diamante(0);
+			}	
+		}
+	}
+}
+
+/*void buscar_horizontal(int posY, int posX, Diamante malla[FIL][COL]){
+	//int b = 1;
+
+	Diamante d_cen = malla[posY][posX];
+	Diamante d_der = malla[posY][posX + 1];
+	Diamante d_izq = malla[posY][posX - 1];
+
+	if((son_iguales(d_cen,d_der) == 0)&&
+		(son_iguales(d_cen,d_izq) == 0)) {
+		//Se eliminan los diamantes cercanos que sean iguales
+
+		malla[posY][posX] = cambiar_diamante(0);
+		malla[posY][posX + 1] = cambiar_diamante(0);
+		malla[posY][posX - 0] = cambiar_diamante(0);
+
+
+
+	}
+	//return b;
+}*/
+int buscar_vertical(int posX, int posY, Diamante malla[FIL][COL]){
+	int b = 1;
+
+	return b;
+}
+
+int main(){
+
+	Diamante  malla[FIL][COL];
+
+	iniciar_malla(malla);
+	rellenar_malla(malla);
+	
+	for (int i = 0; i < 10; ++i)
+	{
+		mostrar_malla(malla);
+		int posY,posX,mov;		
+		printf("PosY: ");
+		scanf("%d", &(posY));
+		printf("PosX: ");
+		scanf("%d", &(posX));
+		printf("Mov{abajo = 0, arriba = 1, izquierda = 2, derecha = 3}: ");
+		scanf("%d", &(mov));
+
+		
+		
+		system("clear");
+		//Refresh
+		mover_diamante(posY,posX,mov,malla);
+		buscar_coincidencias(malla);
+
+
+	}
+
+	return 0;
+}
---
+++ src/joyas.c
@@ -0,0 +1,165 @@
+#include "joyas.h"
+
+/* ------------------ */
+/* VARIABLES GLOBALES */
+/* ------------------ */
+
+/**
+ * Nivel de detalle para los mensajes (para mostrar mensajes de depuración, por ejemplo).
+ */
+int nivel_detalle = 0;
+
+/**
+ * Nivel actual en el juego.
+ */
+int nivel = 1;
+
+/**
+ * Dimensiones de la matriz de juego.
+ *
+ * (por defecto, es de 4 x 5)
+ */
+dim_t tam_matriz = {
+		.filas = 4,
+		.columnas = 5
+	};
+
+/**
+ * Indica si el modo es automático (true) o manual (false).
+ */
+bool modo_auto = true;
+
+
+
+/* -------------------------- */
+/* DECLARACIONES DE FUNCIONES */
+/* -------------------------- */
+
+int procesar_args (int argc, char *argv []);
+void imprimir_info ();
+
+
+
+/* ---------------- */
+/* IMPLEMENTACIONES */
+/* ---------------- */
+
+/**
+ * Función principal.
+ *
+ * @return
+ * 		-> SUCCESS si todo ha acabado correctamente.
+ */
+int main (int argc, char *argv [])
+{
+	int ret = procesar_args (argc, argv);
+
+	if (ret == 0)
+	{
+		/* Imprime los valores de las variables usadas para el juego */
+		imprimir_info ();
+		return SUCCESS;
+	}
+
+	/* Imprime los valores de las variables usadas para el juego */
+	imprimir_info ();
+
+	return SUCCESS;
+}
+
+/**
+ * Procesa los argumentos pasados por línea de comandos
+ *
+ * @param
+ *		Número de argumentos pasados (nº de elementos en argv).
+ *
+ * @param argv
+ *		Array de cadenas con los argumentos.
+ *
+ *
+ * @return
+ *		-> ERR_ARGS si se ha especificado alguna opción no reconocida.
+ *		-> SUCCESS si se han procesado los argumentos correctamente y se
+ *			debe proseguir con la ejecución.
+ *		-> SUCC_ARGS si se ha procesado un argumento y se debe terminar la
+ *			ejecución (p.ej.: tras procesar '-h').
+ */
+int procesar_args (int argc, char *argv [])
+{
+	int res,
+	    aux = 0;
+
+	while ((res = getopt (argc, argv, "hman:f:c:v")) != -1)
+	{
+		switch (res)
+		{
+			case 'h':
+				printf ("%s\n", MSG_AYUDA);
+				return SUCC_ARGS;
+
+			case 'm':
+				modo_auto = false;
+				break;
+
+			case 'a':
+				modo_auto = true;
+				break;
+
+			case 'n':
+				aux = atoi (optarg);
+				/* Si el nivel especificado es menor que o igual a 0,
+				(atoi devuelve 0 si se introduce algo que no es un entero)
+				se establece el nivel 1.
+					Si se indica un nivel mayor que el máximo permitido,
+				se establece el máximo nivel */
+				nivel = (aux <= 0)?
+					1
+					: (aux > MAX_NV)? MAX_NV : aux;
+				break;
+
+			case 'f':
+				aux = atoi (optarg);
+				tam_matriz.filas = (aux <= 0)? 1 : aux;
+				break;
+
+			case 'c':
+				aux = atoi (optarg);
+				tam_matriz.columnas = (aux <= 0)? 1 : aux;
+				break;
+
+			case 'v':
+				/* Aumenta el nivel de detalle */
+				nivel_detalle ++;
+				break;
+
+			default:
+				return ERR_ARGS;
+		}
+	}
+
+	return SUCCESS;
+}
+
+/**
+ * Imprime toda la información de las variables globales del juego.
+ */
+void imprimir_info ()
+{
+	if (nivel_detalle >= 1)
+	{
+		printf ("------------------------------------------\n"
+			"Valor de las variables globales del juego:\n"
+			"\t--> Nivel de detalle: %i\n"
+			"\t--> Dimensiones del juego:\n"
+				"\t\tFilas: %i\n"
+				"\t\tColumnas: %i\n"
+			"\t--> Nivel: %i\n"
+			"\t--> Modo: %s\n"
+			"------------------------------------------\n",
+			nivel_detalle,
+			tam_matriz.filas,
+			tam_matriz.columnas,
+			nivel,
+			(modo_auto)? "automático" : "manual");
+	}
+}
